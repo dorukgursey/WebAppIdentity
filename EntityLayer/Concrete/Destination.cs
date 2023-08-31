d36d5b90584e14664d001e97a1406a263457249f
Doruk Gürsey <dorukgursey@gmail.com>
2023-08-31 16:59:41 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Destination
+    {
+        public int DestinationID { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string DayNight { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/MainAbout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class MainAbout
+    {
+        public int AboutID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string FirstImage { get; set; }
+        public string SecondTitle { get; set; }
+        public string SecondDescription { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
